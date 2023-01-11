293b1e7636596cd59db6db5e6a595d4faf29216f
TheLeftyDog <2b2t@mail.com>
2023-01-11 16:02:55 +0100

    Added 'play' and 'mv' commands

--- app.py
+++ app.py
@@ -12,13 +12,16 @@ def commanderShell():
 @commanderShell.command()
 def help():
     print("""Here is a list of all availible commands:
-    'help' -- Shows this menu,
-    'message' -- Prints a messsage to console,
-    'exit' -- Exits the application
-    'touch <dir>' Creates a file in a specific directory
-    'rm <dir>' Deletes a file in a specific directory
-    'mkdir <dir>' Creates a new directory
-    'rmdir <dir>' Deletes a directory""")
+'help' -- Shows this menu,
+'message' -- Prints a messsage to console,
+'touch <dir>' Creates a file in a specific directory
+'rm <dir>' Deletes a file in a specific directory
+'mkdir <dir>' Creates a new directory
+'rmdir <dir>' Deletes a directory
+'mv <original> <new>' Renames or moves a file or directory
+'play <game>' Play a fun game in the terminal
+'exit' -- Exits the application
+""")
 
 @commanderShell.command()
 @click.argument('message', default='No message provided')
@@ -32,7 +35,7 @@ import os
 @click.argument('directory')
 def touch(directory):
     try:
-        with open(file_directory, 'w') as f:
+        with open(directory, 'w') as f:
             f.write('')
         print("Successfully created file in directory:", directory)
     except Exception as e:
@@ -68,6 +71,45 @@ def rmdir(directory):
     except Exception as e:
         print("Error deleting directory:", directory)
         print(e)
+
+@commanderShell.command()
+@click.argument('original')
+@click.argument('new')
+def mv(original, new):
+    try:
+        os.rename(original, new)
+    except Exception as e:
+        print('Unable to rename or move file/directory')
+        print(e)
+
+@commanderShell.command()
+@click.argument('game')
+def play(game):
+    if game == 'dice':
+        import random
+        dice = str(random.randint(1,6))
+        print('You rolled a '+ dice)
+    elif game == 'quiz':
+        points = 0
+        question = input('What does RAM stand for? ').lower
+        if question == 'random-access memory' or 'random access memory':
+            points += 1
+            print('Correct!')
+        else:
+            print('Incorrect!')  
+        question = input('What is the command to remove files in linux? ')  
+        if question == 'rm':
+            points += 1
+            print('Correct!')
+        if points <1:
+            points = 0
+        points = str(points)
+
+        print('You scored ' + points + 'points!')            
+    else:
+        print("""Unknown game, list of availible games:
+'play dice' Rolls a random number between 1 and 6
+'play quiz' Play a fun computer quiz""")           
 
 if __name__ == '__main__':
     commanderShell()
